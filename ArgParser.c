ad08ba55f4f5d3b67c3b51e550cdee488a37e770
MAGANER <maganer.krik@mail.ru>
2023-10-20 13:33:40 +0300

    module to parse cli arguments

---
+++ ArgParser.c
@@ -0,0 +1,114 @@
+#include"ArgParser.h"
+
+void args_init(args_t* arguments)
+{
+	arguments->mode = no_mode;
+	arguments->type = no_type;
+	arguments->begin = -1;
+	arguments->end = -1;
+}
+bool check_last_ch(char* arg, char ch)
+{
+	if(arg == NULL)return false;
+	
+	size_t len = strlen(arg);
+	return arg[len-1] == ch;
+}
+bool check_first_ch(char* arg,char ch)
+{
+	if(arg == NULL)return false;
+	return arg[0] == ch;
+}
+bool check_is_numeric(char* arg)
+{
+	//don't check last symbol
+	for(int i = 0;i<strlen(arg);++i)
+	{
+		if(!isdigit(arg[i]))
+			return false;
+	}
+	return true;
+} 
+void args_parse(int argc, char** argv, args_t* arguments)
+{
+	for (int i = 1; i< argc; ++i)
+	{
+		char* curr_arg = argv[i];
+		if(strcmp(curr_arg,"--head") == 0)
+		{
+			if(arguments->mode != no_mode)
+			{
+				printf("cut error: mode is already set up!\n");
+				exit(-1);
+			}
+			arguments->mode = head;
+		}
+		else if(strcmp(curr_arg,"--tail") == 0)
+		{
+			if(arguments->mode != no_mode)
+			{
+				printf("cut error: mode is already set up!\n");
+				exit(-1);
+			}
+			arguments->mode = tail;
+		}
+		else if(strcmp(curr_arg,"--range") == 0)
+		{
+			if(arguments->mode != no_mode)
+			{
+				printf("cut error: mode is already set up!\n");
+				exit(-1);
+			}
+			arguments->mode = range;
+		}
+		else if (IS_RANGE(curr_arg))
+		{
+			size_t len = strlen(curr_arg);
+			char* sub = (char*)malloc(sizeof(char)*len);
+			for(int i = 0;i<len-1;++i)
+				sub[i]= curr_arg[i];
+			sub[len-1] = '\0';
+			
+			if(!check_is_numeric(sub))
+			{
+				printf("cut error:%s is not numeric value!\n",sub);
+				exit(-1);
+			}
+
+			if(arguments->begin == -1)
+				arguments->begin = atoi(sub);
+			else if(arguments->end == -1)
+				arguments->end = atoi(sub);
+			else
+			{
+				printf("cut error: range is already set up!\n");
+				exit(-1);
+			}
+			free(sub);
+
+			if(curr_arg[len-1] == 's')
+				arguments->type = str;
+			if(curr_arg[len-1] == 'c')
+				arguments->type = ch;
+			if(curr_arg[len-1] == 'b')
+				arguments->type = byte;						
+		}
+		else
+		{
+			printf("cut error: %s is unknown argument!\n",curr_arg);
+			exit(-1);
+		}		
+	}
+
+	if(arguments->begin == -1 &&
+	   arguments->end   == -1)
+	{
+		printf("cut error: no range specified!\n");
+		exit(-1);   	
+	}
+	if(arguments->begin != -1 &&
+	   arguments->end == -1)
+	{
+		arguments->end = (arguments->begin*2)+1;
+	}
+}
---
+++ ArgParser.h
@@ -0,0 +1,40 @@
+#ifndef ARG_PARSER_H
+#define ARG_PARSER_H
+#include<stdio.h>
+#include<stdbool.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef enum
+{
+	head,
+	tail,
+	range,
+	no_mode
+} arg_mode_t;
+
+typedef enum
+{
+	str,
+	ch,
+	byte,
+	no_type
+} arg_type_t;
+
+typedef struct
+{
+	arg_mode_t mode;
+	arg_type_t type;
+	long int begin, end;
+} args_t;
+
+#define IS_RANGE(x) check_last_ch(x,'s') || check_last_ch(x,'c') || check_last_ch(x,'b')
+static bool check_last_ch(char* arg, char ch);
+static bool check_first_ch(char* arg, char ch);
+static bool check_is_numeric(char* arg);
+
+void args_init(args_t* arguments);
+void args_parse(int argc, char** argv,args_t* arguments);
+
+#endif
